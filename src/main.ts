ec995076b2ffbebef46b7815536a4c96a41ab949
Jayantha <jayantha-p@users.noreply.github.com>
2021-08-10 09:05:10 +0530

    save json file user input

--- src/main.ts
+++ src/main.ts
@@ -1,31 +1,53 @@
 import { Octokit } from "@octokit/rest";
-const octokit = new Octokit({baseUrl: 'https://api.github.com/search/issues?q=author:nalaka type:issue'});
+import readline from "readline";
+var fs = require("fs");
+const octokit = new Octokit({
+  baseUrl: "https://api.github.com/search/issues?q=author:nalaka type:issue",
+});
 
 /**
  * Fetching all the issues
- * @param owner 
- * @param repo 
+ * @param owner
+ * @param repo
  */
-async function getAllIssues(owner:string,repo:string) {
-    try {
-      const {data} = await octokit.rest.issues.listForRepo({
-        owner: owner,
-        repo: repo
-      });;
-  
-      generateJsonFile(data)
-      
-    } catch (error) {
-        console.log("Ërror",error);
-        
-    }
-  }
+async function getAllIssues(owner: string, repo: string) {
 
-function generateJsonFile(data: any) {
-  console.log("Date : ", data);
+
+  try {
+    const { data } = await octokit.rest.issues.listForRepo({
+      owner: owner,
+      repo: repo,
+    });
+
+    generateJsonFile(data);
+  } catch (error) {
+    console.log("Ërror", error);
+  }
 }
+
+function generateJsonFile(jsonData: any) {
+  console.log("Date : ", jsonData);
+  fs.writeFile(
+    "output/test.json",
+    JSON.stringify(jsonData),
+    function (err: Error) {
+      if (err) {
+        console.log(err);
+      }
+    }
+  );
+}
+
+//user input
+// const rl = readline.createInterface({
+//   input: process.stdin,
+//   output: process.stdout,
+// });
+
+// rl.question("message ", (output) => {
+//   console.log(`Hi ${output}!`)
+//   rl.close();
+// });
 
 
 getAllIssues("veracity-dev","github-xlsx");
-
-
