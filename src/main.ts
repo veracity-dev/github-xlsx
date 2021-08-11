a6171b14ca5fb569c6cc1eb3b411f5b6d462fe57
ManuKun <it18043860@my.sliit.lk>
2021-08-11 08:04:03 +0530

    User Input Added

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,23 @@
 import { Octokit } from "@octokit/rest";
-const octokit = new Octokit({baseUrl: 'https://api.github.com/search/issues?q=author:nalaka type:issue'});
+//const octokit = new Octokit({baseUrl: 'https://api.github.com/search/issues?q=author:nalaka type:issue'});
+const octokit = new Octokit({baseUrl: 'https://api.github.com'});
+const prompt = require('prompt-sync')({sigint: true});
 
 /**
  * Fetching all the issues
  * @param owner 
  * @param repo 
  */
+ 
+
+
 async function getAllIssues(owner:string,repo:string) {
     try {
+
+      const repo = prompt('Enter Repo Name: ');
+     //console.log(`Repo name is ${repo}`);
+      const owner = prompt('Enter Owner Name: ');
+      //console.log(`Owner name is ${owner}`);
       const {data} = await octokit.rest.issues.listForRepo({
         owner: owner,
         repo: repo
