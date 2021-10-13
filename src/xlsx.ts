50f9002b0141cdf93be0d9a999c11b246057d6f6
Manujaya <65827662+ManuKun@users.noreply.github.com>
2021-10-13 23:53:54 +0530

    Modifications to default XLSX report (#52)
    Co-authored-by: Praveen Kanishka Gamage coolprav@users.noreply.github.com
    Co-authored-by: Kavinda Keshara Keshara1997@users.noreply.github.com
    Co-authored-by: Rashani Tharushika Wijerathna RashaniTharushika@users.noreply.github.com
    Co-authored-by: Supun Matheesha Suriyaarachchi mathee00@users.noreply.github.com
    Co-authored-by: Chamudi R Wijesekera chamudiRW@users.noreply.github.com

--- src/xlsx.ts
+++ src/xlsx.ts
@@ -4,15 +4,14 @@ export function generateXlsxFile(fileName: string, jsonData: any) {
   const wb = new Excel.Workbook();
   const ws = wb.addWorksheet("Foo");
   var headers = [
-    { header: "TITLE", key: "title", width: 85 },
     { header: "ISSUE_NUMBER", key: "number", width: 20 },
-    { header: "ASSIGNEES", key: "assignee", width: 150 },
-    { header: "LABEL", key: "label", width: 35 },
-    { header: "MILESTONE", key: "milestone", width: 35 },
-    { header: "AUTHOR", key: "author", width: 20 },
-    { header: "BODY", key: "body" },
     { header: "STATE", key: "state" },
+    { header: "ASSIGNEES", key: "assignee", width: 150 },
+    { header: "TITLE", key: "title", width: 85 },
+    { header: "UPDATED_AT", key: "updatedAt", width: 25 },
     { header: "CREATED_AT", key: "createdAt", width: 25 },
+    { header: "LABEL", key: "labels", width: 20 },
+    { header: "BODY", key: "body" },
   ];
   ws.columns = headers;
   ws.addRows(jsonData.map((x: any) => hyperlinkToTitle(x)));
